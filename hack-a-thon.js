b1bcf5c5050a02ef9b116c8407ebe6385a93ef34
reddy-manjunath <149348214+reddy-manjunath@users.noreply.github.com>
2024-03-23 12:50:00 +0530

    Add files via upload

---
+++ hack-a-thon.js
@@ -0,0 +1,23 @@
+const nextPageButton = document.getElementById("shimla");
+
+nextPageButton.addEventListener("click", function() {
+  window.location.href = "shimla.html"; 
+});
+const next1PageButton = document.getElementById("egypt");
+
+next1PageButton.addEventListener("click", function() {
+  window.location.href = "egypt.html"; 
+});
+const next2PageButton = document.getElementById("sahara");
+
+next2PageButton.addEventListener("click", function() {
+  window.location.href = "sahara.html"; 
+});
+const next3PageButton = document.getElementById("dubai");
+
+next3PageButton.addEventListener("click", function() {
+  window.location.href = "dubai.html"; 
+});
+
+
+
